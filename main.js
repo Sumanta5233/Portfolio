1656670e89d301579aa9e1738089159af1fc72b4
Sumanta Mondal <142692108+Sumanta5233@users.noreply.github.com>
2023-10-12 17:46:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text", {
-    strings: ["Frontend Developer" , "Web developer" , "Game Lover"],
+    strings: ["Frontend Developer" , "Web developer" ],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
